d5fba68b0038152817c764b36a2efc093734087a
wql70255 <48751075+wql70255@users.noreply.github.com>
2021-08-09 13:08:01 +0800

    胖虎修复自动领取问题

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -35,9 +35,15 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
                    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'olmijoxgmjutyvo33dqcolstdty3m2gipwiwfti@olmijoxgmjutyrsovl2xalt2tbtfmg6sqldcb3q@e7lhibzb3zek27amgsvywffxx7hxgtzstrk2lba@e7lhibzb3zek32e72n4xesxmgc2m76eju62zk3y@l4ex6vx6yynovp6l5zmgzx4nssii54ewecu36gi@l4ex6vx6yynovp6l5zmgzx4nssii54ewecu36gi',
+  'qzk5udvyj5o5oqdbvo3gjkvm4rdbumunmxvvtlq@mlrdw3aw26j3xyycgphuam3rbm3nnjpkjwwx7ha',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  '47m36n7ro5gutdd32u2grn4q5rboku36cstfm6a@mlrdw3aw26j3wgzjipsxgonaoyr2evrdsifsziyvnsb2r54jq34s64sc4it3jlfnejwmtmsuadax2i@eeexxudqtlampbpvmceutaaht5tcftvr6kohuny@e7lhibzb3zek27gfeceqb6wwm45gshcaroxg5ka@e7lhibzb3zek3xxnrskw4mpzstihpk3f7fqziiy@olmijoxgmjutzhazczrfgf75qrbqseqdmb5ey5a',
+    'qzk5udvyj5o5oqdbvo3gjkvm4rdbumunmxvvtlq@mlrdw3aw26j3xyycgphuam3rbm3nnjpkjwwx7ha',
+  'qzk5udvyj5o5oqdbvo3gjkvm4rdbumunmxvvtlq@mlrdw3aw26j3xyycgphuam3rbm3nnjpkjwwx7ha',
+  'qzk5udvyj5o5oqdbvo3gjkvm4rdbumunmxvvtlq@mlrdw3aw26j3xyycgphuam3rbm3nnjpkjwwx7ha',
+  'qzk5udvyj5o5oqdbvo3gjkvm4rdbumunmxvvtlq@mlrdw3aw26j3xyycgphuam3rbm3nnjpkjwwx7ha',
+  'qzk5udvyj5o5oqdbvo3gjkvm4rdbumunmxvvtlq@mlrdw3aw26j3xyycgphuam3rbm3nnjpkjwwx7ha',
+  'qzk5udvyj5o5oqdbvo3gjkvm4rdbumunmxvvtlq@mlrdw3aw26j3xyycgphuam3rbm3nnjpkjwwx7ha',
+  'qzk5udvyj5o5oqdbvo3gjkvm4rdbumunmxvvtlq@mlrdw3aw26j3xyycgphuam3rbm3nnjpkjwwx7ha'
 ]
 let allMessage = ``;
 let currentRoundId = null;//本期活动id
