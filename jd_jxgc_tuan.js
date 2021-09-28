430e72c62ca81d79f75525b8f3a4bf1fa338c74b
wql70255 <48751075+wql70255@users.noreply.github.com>
2021-09-28 12:53:26 +0800

    Update jd_jxgc_tuan.js

--- jd_jxgc_tuan.js
+++ jd_jxgc_tuan.js
@@ -96,7 +96,7 @@ if ($.isNode()) {
       if ((cookiesArr && cookiesArr.length >= ($.tuanNum || 5)) && $.canHelp) {
         for (let j = 0; j < $.tuanIds.length; j++) {
           let item = $.tuanIds[j];
-          if ($.index == 1) {item = 'Qi3lX9-jBTTNpRSLdOx1Eg=='}
+          if ($.index == 1) {item = '4rZ7fNH-gpN3ypnj9EQevg=='}
           $.tuanMax = false;
           if (!$.canHelp) break;
           console.log(`账号${$.UserName} 去参加团 ${item}`);
